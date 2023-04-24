2b013a25ef607033f8c6ae522b102f3dd209a112
Fabian-Hillary <fabosome@gmail.com>
2023-04-24 11:34:53 -0700

    main.h

--- main.h
+++ main.h
@@ -22,4 +22,11 @@ int get_f(const char *format, int *i);
 int get_p(const char *format, int *i, va_list args);
 int get_s(const char *format, int *i);
 int get_w(const char *format, int *i, va_list args);
+int handle_print(const char *fmt, int *ind, va_list list,
+                char buff[], int f, int w, int p, int size);
+int is_printable(char x);
+int append_hexa_code(char ascii_code, char buff[], int i);
+int is_digit(char c);
+long int convert_size_num(long int num, int size);
+long int convert_size_unsigned(unsigned long int num, int size);
 #endif 
